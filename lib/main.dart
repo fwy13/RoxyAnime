87a2e3f2e2b8c828cad4c0a679cc5838e523a10d
fwy13 <quyhuynh1309@gmail.com>
2025-05-16 14:28:20 +0700

    chore: update ui

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('xin chào'),
+          child: Text('xin chào từ fwy13'),
         ),
       ),
     );
